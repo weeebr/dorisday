f0209107b3e75ae01c37f4442576c931dbf84d2c
Pascal Weber <pascal.weber@ringier.ch>
2024-12-24 13:13:37 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [react()],
+  server: {
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  },
 });
