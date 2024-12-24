77eec3f9efb7b8ae52ce32b0b95b3f415e920ce4
Pascal Weber <pascal.weber@ringier.ch>
2024-12-24 18:29:07 +0100

    wup wup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import path from 'path';
 
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
