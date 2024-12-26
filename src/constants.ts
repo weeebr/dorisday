bd8328bba12ea513138e8cb66f0261915da5c85c
Pascal Weber <pascal.weber@ringier.ch>
2024-12-26 12:19:59 +0100

    improve navigation

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+  { href: '#kollektionen', text: 'KOLLEKTIONEN' },
+  { href: '#cafe', text: 'JUNGLE CAFÉ' },
+  { href: '#kultur', text: 'KULTUR' },
+  { href: '#contact', text: 'KONTAKT' },
+];
